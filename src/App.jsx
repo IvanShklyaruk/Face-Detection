c0c3b1c258448d354d4ec67e77e65f487d081f0a
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-22 00:52:19 -0400

    face detection initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import FaceDetectionPage from "./pages/FaceDetectionPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <FaceDetectionPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
